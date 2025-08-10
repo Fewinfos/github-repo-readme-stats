7d5bae185074f541ce81e575873dc808384e7d85
KUMARESAN C <kumaresankavi07@gmail.com>
2025-08-10 19:10:46 +0530

    added  a theme folder with index.js

--- themes/index.js
+++ themes/index.js
@@ -1,2 +1,10 @@
-export * from './lightTheme';
-export * from './darkTheme';
+const theme = {
+  title_color: "#2f80ed",
+  icon_color: "#4c71f2",
+  text_color: "#434d58",
+  bg_color: "#fffefe",
+  border_color: "#e4e2e2"
+};
+
+module.exports = theme;
+export default theme;
